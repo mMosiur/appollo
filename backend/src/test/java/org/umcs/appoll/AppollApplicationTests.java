9dacff233539708de5016c347b05d42c6eaac8ef
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-05-12 19:19:33 +0200

    Added project template

---
+++ backend/src/test/java/org/umcs/appoll/AppollApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umcs.appoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
