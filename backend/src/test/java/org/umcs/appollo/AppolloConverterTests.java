67514392b3c1cad178842d349c5a949933b1e7c7
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-05-31 19:09:12 +0200

    Initial commit;

--- backend/src/test/java/org/umcs/appollo/AppolloConverterTests.java
+++ backend/src/test/java/org/umcs/appollo/AppolloConverterTests.java
@@ -1,6 +1,8 @@
 package org.umcs.appollo;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.tomcat.jni.Poll;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,7 @@ import org.umcs.appollo.model.api.QuestionDetails;
 import org.umcs.appollo.model.api.PollDetails;
 import org.umcs.appollo.model.api.PollLabel;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -32,14 +35,13 @@ public class AppolloConverterTests {
     private final int pollId = 0, questionId = 1, answerId = 2;
 
     private PollEntity pollEntity;
-    private PollLabel pollLabel;
     private PollDetails pollDetails;
     private final String pollName = "Example poll.";
 
     private QuestionEntity questionEntity;
     private QuestionDetails questionDetails;
     private final String questionText = "What's your favorite color?", questionType = "text";
-    private final String questionOptions = gson.toJson(new String[]{
+    private final String questionOptions = gson.toJson(new String[] {
             "Red", "Green", "Blue"
     });
 
@@ -57,6 +59,10 @@ public class AppolloConverterTests {
         questionEntity = new QuestionEntity();
         pollEntity = new PollEntity();
 
+        answerDetails = new AnswerDetails();
+        questionDetails = new QuestionDetails();
+        pollDetails = new PollDetails();
+
         user1 = new UserEntity();
         user2 = new UserEntity();
 
@@ -64,34 +70,50 @@ public class AppolloConverterTests {
         pollEntity.setId(pollId);
         pollEntity.setName(pollName);
         pollEntity.setUser(user1);
+        pollDetails.setId(pollId);
+        pollDetails.setName(pollName);
+        // TODO: no method?
+//        pollDetails.setUser(user1);
 
         // Model a question
         questionEntity.setId(questionId);
         questionEntity.setText(questionText);
         questionEntity.setOptions(questionOptions);
         questionEntity.setType(questionType);
+        questionDetails.setId(questionId);
+        questionDetails.setText(questionText);
+        questionDetails.setOptions(gson.fromJson(questionOptions, new TypeToken<ArrayList<String>>(){}.getType()));
+        questionDetails.setType(QuestionDetails.TypeEnum.TEXT);
 
         // Model an answer
         answerEntity.setId(answerId);
         answerEntity.setUser(user2);
         answerEntity.setAnswerJson(answerJson);
+        answerDetails.setId(answerId);
+        answerDetails.setAnswerJson(answerJson);
 
         // Connect relations
-        List<QuestionEntity> questions = new LinkedList<>();
-        questions.add(questionEntity);
-        List<AnswerEntity> answers = new LinkedList<>();
-        answers.add(answerEntity);
+        List<QuestionEntity> questionEntitiesList = new LinkedList<>();
+        questionEntitiesList.add(questionEntity);
+        List<QuestionDetails> questionDetailsList = new LinkedList<>();
+        questionDetailsList.add(questionDetails);
+        List<AnswerEntity> answerEntitiesList = new LinkedList<>();
+        answerEntitiesList.add(answerEntity);
+        List<AnswerDetails> answerDetailsList = new LinkedList<>();
+        answerDetailsList.add(answerDetails);
 
-        pollEntity.setQuestions(questions);
+        pollEntity.setQuestions(questionEntitiesList);
+        pollDetails.setQuestions(questionDetailsList);
         questionEntity.setPoll(pollEntity);
-        questionEntity.setAnswers(answers);
+        questionEntity.setAnswers(answerEntitiesList);
         answerEntity.setQuestion(questionEntity);
+        answerDetails.setQuestionId(questionId);
     }
 
     @Test
     public void ConvertPollFromEntityToApi() {
         Assert.assertNull(pollConverter.FromEntityToApi(null));
-        pollLabel = pollConverter.FromEntityToApi(pollEntity);
+        PollLabel pollLabel = pollConverter.FromEntityToApi(pollEntity);
         Assert.assertEquals(pollLabel.getId().intValue(), pollId);
         Assert.assertEquals(pollLabel.getName(), pollName);
     }
@@ -99,7 +121,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertQuestionFromEntityToApi() {
         Assert.assertNull(questionConverter.FromEntityToApi(null));
-        questionDetails = questionConverter.FromEntityToApi(questionEntity);
+        QuestionDetails questionDetails = questionConverter.FromEntityToApi(questionEntity);
         Assert.assertEquals(questionDetails.getId().intValue(), questionId);
         Assert.assertEquals(gson.toJson(questionDetails.getOptions()), questionOptions);
         Assert.assertEquals(questionDetails.getText(), questionText);
@@ -109,7 +131,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertPollFromEntityToApiDetailed() {
         Assert.assertNull(pollConverter.FromEntityToApiDetailed(null));
-        pollDetails = pollConverter.FromEntityToApiDetailed(pollEntity);
+        PollDetails pollDetails = pollConverter.FromEntityToApiDetailed(pollEntity);
         Assert.assertEquals(pollDetails.getId().intValue(), pollId);
         Assert.assertEquals(pollDetails.getName(), pollName);
         Assert.assertEquals(pollDetails.getIsActive(), true);
@@ -120,7 +142,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertAnswerFromEntityToApi() {
         Assert.assertNull(answerConverter.FromEntityToApi(null));
-        answerDetails = answerConverter.FromEntityToApi(answerEntity);
+        AnswerDetails answerDetails = answerConverter.FromEntityToApi(answerEntity);
         Assert.assertEquals(answerDetails.getId().intValue(), answerId);
         Assert.assertEquals(answerDetails.getQuestionId().intValue(), questionId);
         Assert.assertEquals(answerDetails.getAnswerJson(), answerJson);
@@ -129,7 +151,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertPollFromApiToEntity() {
         Assert.assertNull(questionConverter.FromApiToEntity(null));
-        pollEntity = pollConverter.FromApiToEntity(pollDetails);
+        PollEntity pollEntity = pollConverter.FromApiToEntity(pollDetails);
         Assert.assertEquals(pollEntity.getId().intValue(), pollId);
         Assert.assertEquals(pollEntity.getName(), pollName);
         Assert.assertEquals(pollEntity.getUser(), user1);
@@ -140,7 +162,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertQuestionFromApiToEntity() {
         Assert.assertNull(questionConverter.FromEntityToApi(null));
-        questionEntity = questionConverter.FromApiToEntity(questionDetails);
+        QuestionEntity questionEntity = questionConverter.FromApiToEntity(questionDetails);
         Assert.assertEquals(questionEntity.getId().intValue(), questionId);
         Assert.assertEquals(questionEntity.getOptions(), questionOptions);
         Assert.assertEquals(questionEntity.getText(), questionText);
@@ -153,7 +175,7 @@ public class AppolloConverterTests {
     @Test
     public void ConvertAnswerFromApiToEntity() {
         Assert.assertNull(answerConverter.FromApiToEntity(null));
-        answerEntity = answerConverter.FromApiToEntity(answerDetails);
+        AnswerEntity answerEntity = answerConverter.FromApiToEntity(answerDetails);
         Assert.assertEquals(answerEntity.getId().intValue(), answerId);
         Assert.assertEquals(answerEntity.getQuestion(), questionEntity);
         Assert.assertEquals(answerEntity.getAnswerJson(), answerJson);
