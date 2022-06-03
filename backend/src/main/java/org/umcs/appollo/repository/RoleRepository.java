c7429fb2a1b8625e604b8dafb5c43b2b015d86d5
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-03 10:22:37 +0200

    Fixed RoleRepository to extend JpaRepository based on RoleEntity type, not UserEntity;

--- backend/src/main/java/org/umcs/appollo/repository/RoleRepository.java
+++ backend/src/main/java/org/umcs/appollo/repository/RoleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.umcs.appollo.model.RoleEntity;
 import org.umcs.appollo.model.UserEntity;
 
-public interface RoleRepository extends JpaRepository<UserEntity, Integer> {
+public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
     @Query( value = "SELECT r from RoleEntity r where r.name = ?1")
     RoleEntity findByName(String name);
 }
