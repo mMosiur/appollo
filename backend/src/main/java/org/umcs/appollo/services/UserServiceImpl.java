c6551bd05bc4178784e59449cc9c1f77ce3cc4bc
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-06-09 12:43:37 +0200

    Attempting to create user with already taken username/email will result in 409 with info about which is taken;

--- backend/src/main/java/org/umcs/appollo/services/UserServiceImpl.java
+++ backend/src/main/java/org/umcs/appollo/services/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.umcs.appollo.configuration.RoleNames;
 import org.umcs.appollo.converters.UserConverter;
+import org.umcs.appollo.exceptions.ConflictException;
 import org.umcs.appollo.exceptions.ResourceNotFoundException;
 import org.umcs.appollo.model.RoleEntity;
 import org.umcs.appollo.model.UserEntity;
@@ -57,6 +58,14 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     @Override
     public User addNewUser(User user) {
         UserEntity userEntity = userConverter.FromApiToEntity(user);
+
+        UserEntity a = userRepository.findByUsername(user.getUsername());
+        if (a != null)
+            throw new ConflictException("Username is already taken.");
+        a = userRepository.findByEmail(user.getEmail());
+        if (a != null)
+            throw new ConflictException("Email is already taken.");
+
         userEntity.setId(null);
         userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         Set<RoleEntity> roles = new HashSet<>();
